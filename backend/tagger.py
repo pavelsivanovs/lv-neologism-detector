7f74cc9959fc5c2a58276867c0f7611d754b1ff5
Pavels Ivanovs <32202454+pavelsivanovs@users.noreply.github.com>
2023-03-26 22:42:23 +0300

    Substituted `subprocess` lib with `pexpect` for encapsulating LVTagger

--- backend/tagger.py
+++ backend/tagger.py
@@ -1,34 +1,47 @@
 import os.path
-from subprocess import PIPE, Popen
+import time
+from os import fpathconf
 from typing import List
+
+from pexpect import EOF, TIMEOUT, spawn
 
 
 class Lemmatizer:
     """ Encapsulation class for a LVTagger Morphotagging method
-    implemented in Peterisp/LVTagger (https://github.com/PeterisP/LVTagger). """
-
-    # FIXME: don't work for multiple inputs. can try `pexpect`:
-    #  https://stackoverflow.com/questions/28616018/multiple-inputs-and-outputs-in-python-subprocess-communicate
+    implemented in PeterisP/LVTagger (https://github.com/PeterisP/LVTagger). """
 
     def __init__(self):
         morphotagger_script_path = os.path.abspath(os.path.join(os.path.curdir, 'tagger', 'morphotagger.sh'))
         command = ['bash', morphotagger_script_path, '-lemmatized-text']
-        self.lemmatizer = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
+        self._lemmatizer = spawn(' '.join(command), timeout=None, encoding='UTF-8')
+
+        self._cpl = self._lemmatizer.compile_pattern_list('\r\n.*\r\n\r\n')
+        self._byte_limit_per_line = fpathconf(0, 'PC_MAX_CANON')
+        self._lemmatizer.expect(r'done \[.* sec\]\.\r\n')
+
+        print(f'Number of bytes that can be received by line is {self._byte_limit_per_line}')
+        # TODO set logging
 
     def __del__(self):
-        self.lemmatizer.kill()
+        self._lemmatizer.sendeof()
 
     def lemmatize(self, text: str) -> List[str]:
         if text == '':
             raise Exception('Unintentional termination of the lemmatizer process by passing an empty string.')
-        output, error = self.lemmatizer.communicate(text)
 
-        # Commenting out because retrieved error is related to the LVTagger
-        # if error:
-        #     raise Exception(f'''
-        #     Error occurred during lemmatization of text: {text}
-        #     Retrieved output: {output}
-        #     Error: {error}
-        #     ''')
+        try:
+            self._lemmatizer.sendline(text)
+            self._lemmatizer.expect_list(self._cpl, timeout=5)
+            output = self._lemmatizer.after
+            return output.split()
+        except EOF or TIMEOUT as exception:
+            print(exception)
+            raise exception
 
-        return output.split()
+
+if __name__ == '__main__':
+    lem = Lemmatizer()
+    print(lem.lemmatize('Šis ir testa ievada teikums valodas apstrādes rīkam.'))
+    time.sleep(31)
+    print(lem.lemmatize('es uzdāvināju draudzenei Lego ziedu pušķa konstruktoru'))
+    # print(lem._lemmatizer)
