a50370af62de412b3f7e6b43f4e5a469f435eca9
Pavels Ivanovs <32202454+pavelsivanovs@users.noreply.github.com>
2023-05-04 10:41:13 +0300

    Updates to db.py

--- backend/utils/db.py
+++ backend/utils/db.py
@@ -3,14 +3,19 @@ from contextlib import contextmanager
 
 import psycopg
 from dotenv import load_dotenv
+from psycopg import Cursor, ServerCursor
+
+from utils.logger import get_configured_logger
+
+logger = get_configured_logger(__name__)
 
 local_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../.env')
 if os.path.isfile(local_file_path):
-    print(f'Loading environment from {local_file_path}')
+    logger.info(f'Loading environment from {local_file_path}')
     load_dotenv(local_file_path, verbose=True)
 
 
 @contextmanager
-def db_cursor():
+def db_cursor() -> Cursor | ServerCursor:
     with psycopg.connect(os.getenv('DB_URL')) as conn:
         yield conn.cursor()
