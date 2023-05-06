e96509599e92b7b7ea957c474a871e6122a3faa0
Pavels Ivanovs <32202454+pavelsivanovs@users.noreply.github.com>
2023-05-07 00:05:31 +0300

    Adding logging output to a dir

--- backend/utils/logger.py
+++ backend/utils/logger.py
@@ -1,14 +1,22 @@
 import logging
+import os.path
 
 from logging import Logger
 
 
 def get_configured_logger(module_name: str) -> Logger:
-    handler = logging.FileHandler('traversing_commoncrawl.log')
+    utils_dir = os.path.abspath(os.path.dirname(__file__))
+    if not os.path.exists(log_dir := os.path.join(utils_dir, '..', 'logs')):
+        os.makedirs(log_dir)
+    path = os.path.join(log_dir, 'traversing_commoncrawl.log')
+    handler = logging.FileHandler(path)
     formatter = logging.Formatter('%(asctime)s : %(name)s : %(levelname)s : %(message)s')
     handler.setFormatter(formatter)
+
     logging.basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', )
     logger = logging.getLogger(module_name)
     logger.setLevel(logging.DEBUG)
+
     logger.addHandler(handler)
+
     return logger
